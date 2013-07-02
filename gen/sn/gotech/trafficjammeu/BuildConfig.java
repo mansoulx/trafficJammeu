eed5b74ad7b0c890eb37378cacf937952a752f83
Souleymane Thiongane <mansoulx@hotmail.com>
2013-07-02 09:53:28 +0100

    first comit

---
+++ gen/sn/gotech/trafficjammeu/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package sn.gotech.trafficjammeu;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/sn/gotech/trafficjammeu/MapActivity.java
@@ -0,0 +1,13 @@
+package sn.gotech.trafficjammeu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(sn.gotech.trafficjammeu.R.layout.main);
+    }
+}
